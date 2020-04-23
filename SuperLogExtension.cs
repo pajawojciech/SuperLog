ce1a722c5c1a3f7a07adb5a2483e58fd1f6d5361
Wojciech Paja <wpaja@9bits.pl>
2020-04-23 18:36:30 +0200

    Dapper - decimal, datetime, table parameter

--- SuperLogExtension.cs
+++ SuperLogExtension.cs
@@ -1,13 +1,15 @@
-public static class SuperLogExtension /*v17*/
+public static class SuperLogExtension /*v18.1*/
 {
-    private static void tr(object t, int i = 0, string s = "")
+    private static System.Action<string> ts = (string st) =>
     {
-        System.Action<string> ts = (string st) =>
-        {
-            System.Diagnostics.Trace.WriteLine(st);
-        };
+        System.Diagnostics.Trace.WriteLine(st);
+    };
+
+    private static void tr(object t, int i = 0, string s = "", object pp = null)
+    {
         var sb = new System.Text.StringBuilder();
         var sb2 = new System.Text.StringBuilder();
+        int pPos = 0;
         if (i != 0)
         {
             for (int j = 0; j < i; j++)
@@ -173,7 +175,34 @@ public static class SuperLogExtension /*v17*/
                     else if (type == typeof(bool)) sb.AppendFormat("DECLARE @{0} BIT = {1}\n", name, (bool)pValue ? 1 : 0);
                     else if (type == typeof(int)) sb.AppendFormat("DECLARE @{0} INT = {1}\n", name, pValue);
                     else if (type == typeof(System.Collections.Generic.List<int>)) sb.AppendFormat("-- REPLACE @{0} IN SQL: ({1})\n", name, string.Join(",", (System.Collections.Generic.List<int>)pValue));
-                    else if (type == typeof(decimal) || type == typeof(double)) sb.AppendFormat("DECLARE @{0} DECIMAL(15,2) = {1}\n", name, pValue);
+                    else if (type == typeof(decimal) || type == typeof(double)) sb.AppendFormat("DECLARE @{0} DECIMAL({2},{3}) = {1}\n", name, pValue.ToString().Replace(",", "."), pValue.ToString().Length - 1, pValue.ToString().Split(",")[1].Length);
+                    else if (type.ToString() == "Dapper.TableValuedParameter")
+                    {
+                        if (pp is Array)
+                        {
+                            if ((pp as Array).Length > pPos)
+                            {
+                                var obj = (pp as Array).GetValue(pPos);
+                                if ((pp as Array).Length > pPos + 1 && (pp as Array).GetValue(pPos + 1) is string)
+                                {
+                                    sb.AppendFormat("\nDECLARE @{0} {1} \n", name, (pp as Array).GetValue(pPos + 1).ToString());
+                                    pPos++;
+                                }
+                                else
+                                {
+                                    sb.AppendFormat("\nDECLARE @{0} dbo.TYPE \n", name);
+
+                                }
+                                sb.Append(tp(obj, name));
+                                pPos++;
+                            }
+                        }
+                        else
+                        {
+                            sb.AppendFormat("\nDECLARE @{0} dbo.TYPE \n", name);
+                            sb.Append(tp(pp, name));
+                        }
+                    }
                     else sb.AppendFormat("DECLARE @{0} NVARCHAR(MAX) = '{1}'\n", name, pValue.ToString());
                 }
                 ts(sb.ToString());
@@ -196,9 +225,67 @@ public static class SuperLogExtension /*v17*/
             ts("NULL");
         }
     }
-    public static object log(this object t, string s = "")
+
+    private static string tp(object t, string name)
     {
-        tr(t, 0, s);
+        if (t == null) return "";
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        bool firstRow = true;
+        int i = 1;
+        dynamic td = t;
+        foreach (object y in td)
+        {
+            if (firstRow)
+            {
+                sb.AppendFormat("insert into @{0} values", name);
+            }
+            else
+            {
+                sb.Append(",");
+            }
+            sb.Append('(');
+
+            bool firstCol = true;
+            foreach (System.Reflection.PropertyInfo prop in y.GetType().GetProperties())
+            {
+                if (!firstCol)
+                {
+                    sb.Append(',');
+                }
+                object x = prop.GetValue(y);
+                if (x is null)
+                {
+                    sb.Append("NULL");
+                }
+                else if (x is decimal || x is double || x is int)
+                {
+                    sb.Append($@"{x.ToString().Replace(',', '.')}");
+                }
+                else if (x is DateTime)
+                {
+                    sb.Append("'" + ((DateTime)x).ToString("yyyy-MM-dd HH:mm:ss.fff") + "'");
+                }
+                else
+                {
+                    sb.Append($@"'{x}'");
+                }
+                firstCol = false;
+            }
+            sb.Append(')');
+            firstRow = false;
+            if (++i == 1000)
+            {
+                firstRow = true;
+                i = 1;
+                sb.AppendLine();
+            }
+        }
+        sb.AppendLine();
+        return sb.ToString();
+    }
+    public static object log(this object t, string s = "", object p = null)
+    {
+        tr(t, 0, s, p);
         return t;
     }
 }
